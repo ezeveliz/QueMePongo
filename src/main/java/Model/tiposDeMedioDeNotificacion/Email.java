7b876e332688744ec1339a42e96fca9475bbd701
yairtele <yairtele@yahoo.com.ar>
2019-12-23 19:23:41 -0300

    Deploy

--- src/main/java/Model/tiposDeMedioDeNotificacion/Email.java
+++ src/main/java/Model/tiposDeMedioDeNotificacion/Email.java
@@ -26,7 +26,7 @@ public class Email implements MedioDeNotificacion {
     static int getSMTPPort() {
         //ProcessBuilder processBuilder = new ProcessBuilder();
         if (System.getenv("SMTP_PORT") != null) {
-            return 0;//System.getenv("SMTP_PORT");
+            return Integer.parseInt(System.getenv("SMTP_PORT"));
         }
         return 587; //return default smtp port if isn't set
     }
